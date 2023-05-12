2eb1fd10f6617c8827946fdf41bdc6552b3270e9
Waleed1123 <waleed007896@gmail.com>
2023-05-12 17:57:10 +0500

    Complete but Del not Working

--- app.js
+++ app.js
@@ -12,5 +12,54 @@ function addTodo(params) {
   liElement.appendChild(liTxt);
   liElement.className = "list-group-item d-flex justify-content-between align-items-center";
 
-  console.log(liElement)
+  var div = document.createElement("div");
+  var editBtn = document.createElement("button")
+  var deleteBtn = document.createElement("button")
+  editBtn.innerHTML = "Edit";
+  
+  // SET ONCLICK EVENT
+  editBtn.setAttribute("onclick", "editTodo(this)");
+  deleteBtn.innerHTML= "Delete";
+
+  deleteBtn.setAttribute("onclick" , "deleteTodo()");
+
+  editBtn.className= "btn btn-info "
+  deleteBtn.className= "btn btn-danger"
+  div.appendChild(editBtn);
+  div.appendChild(deleteBtn);
+
+  
+  
+
+
+
+  liElement.appendChild(div)
+  
+  
+  ulParent.appendChild(liElement)
+
+  input.value = ""
+
+
+
 };
+
+
+function editTodo(el){
+
+  var li = el.parentNode.parentNode
+
+  var placeHolder = li.firstChild.nodeValue
+  var editValue = prompt("Edit ToDo.", placeHolder)
+  console.log(editValue)
+
+  li.firstChild.nodeValue = editValue;
+};
+
+function deleteBtn(elem){
+  elem.parentNode.parentNode.remove()
+};
+
+function deleteAll(){
+  ulParent.innerHTML = ""
+}
